c70ec870f2d32478b94baaa38c1dbac58868264c
Srinivas <kookatlasrinivasyadav@gmail.com>
2022-07-25 21:54:33 +0530

    updated cofig file base package

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
                       .select()
                       .paths(PathSelectors.any())
-                      .apis(RequestHandlerSelectors.basePackage("in.srinivas.rest"))
+                      .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
                       .build();
                     		  
 	}
